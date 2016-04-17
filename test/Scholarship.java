acd3694832169f7ed38866cadab619cf1fe67e56
thanhnhas <nhant795@gmail.com>
2016-04-17 16:59:34 +0700

    chng

---
+++ test/Scholarship.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nguyen Thanh Nha
+ */
+public class Scholarship {
+    private double gpa;
+
+    public Scholarship(double gpa) {
+        this.gpa=gpa;
+    }
+
+    public int getAward() {
+        if(gpa>4.0  || gpa <0){
+            return -1;
+        }
+        if(gpa>=3.7){
+            return 100;
+        }else if(gpa>=3.6){
+            return 80;
+        }else if(gpa>=3.4){
+            return 70;
+        }else{
+            return 0;
+        }
+    }
+
+    
+    
+}
